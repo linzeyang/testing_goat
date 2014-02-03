4fd23f3ed6c1db887c3b8a31980ccbedab97583b
linzeyang <zeyanglin2013@gmail.com>
2014-02-03 17:31:17 +0000

    Chapter.3

--- functional_tests.py
+++ functional_tests.py
@@ -1,26 +1,29 @@
-from selenium import webdriver
 import unittest
 
-class NewVisitorTest(unittest.TestCase): #1
+from selenium import webdriver
 
-    def setUp(self): #2
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    def tearDown(self): #3
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self): #4
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title) #5
-        self.fail('Finish the test!') #6
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
         #[...rest of comments as before]
 
-if __name__ == '__main__': #7
-    unittest.main() #8
+
+if __name__ == '__main__':
+    unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,6 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resove('/')
+        found = resolve('/')
         self.assertEqual(found.func, home_page)
 
