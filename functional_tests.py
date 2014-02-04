ca4de06fae8a709a60869ac1f2e03a8d93e2f14d
linzeyang <zeyanglin2013@gmail.com>
2014-02-04 16:36:24 +0000

    Chapter.5 Mid

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -35,10 +35,10 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
 
+        #time.sleep(3)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
