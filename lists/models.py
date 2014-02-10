8d4e651d1a95bf282dfbf1933f169fb1e4ae34c3
linzeyang <zeyanglin2013@gmail.com>
2014-02-10 18:24:10 +0000

    Chapter.6 near End

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     """docstring for Item"""
     text = models.TextField()
+    list = models.ForeignKey(List)
         
